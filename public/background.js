21621a89e8f84775c9f959db3d4c916379d904fb
Alfxjx <xjxtju@163.com>
2020-07-09 17:03:25 +0800

    bing success

--- public/background.js
+++ public/background.js
@@ -1,20 +1,49 @@
-function _getBackgroundImageFromBing() {
+async function _getBackgroundImageFromBing() {
   const apiLink = "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1";
-  const bing = "https://cn.bing.com";
+  let bing = "https://cn.bing.com";
   // let ImgUrl = axios.get(apiLink).then((res) => {
   //   let retlinks = res["images"][0]["url"];
   //   return retlinks;
   // });
+  let ImgUrl = await fetch(apiLink)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      console.log(myJson);
+      return myJson;
+    });
+  console.log(ImgUrl.images[0].url);
 
-  let ImgUrl = fetch(apiLink).then((res) => {
-    console.log(res);
-    let json = res.json();
-    return json;
-  });
-  // TODO 有问题
-  console.log(ImgUrl);
-  console.log(ImgUrl["images"][0]["url"]);
-
-  let bgLink = bing + ImgUrl;
+  let bgLink = bing + ImgUrl.images[0].url;
   return bgLink;
 }
+
+// {
+//   "images": [
+//   {
+//   "startdate": "20200708",
+//   "fullstartdate": "202007081600",
+//   "enddate": "20200709",
+//   "url": "/th?id=OHR.ColoradoColumbine_ZH-CN0901580141_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp",
+//   "urlbase": "/th?id=OHR.ColoradoColumbine_ZH-CN0901580141",
+//   "copyright": "位于科罗拉多南部圣胡安山脉的美国盆地 (© Blaine Harrington III/Alamy)",
+//   "copyrightlink": "https://www.bing.com/search?q=%E5%9C%A3%E8%83%A1%E5%AE%89%E5%B1%B1%E8%84%89&form=hpcapt&mkt=zh-cn",
+//   "title": "",
+//   "quiz": "/search?q=Bing+homepage+quiz&filters=WQOskey:%22HPQuiz_20200708_ColoradoColumbine%22&FORM=HPQUIZ",
+//   "wp": true,
+//   "hsh": "2bca1e176b879f5b4839ac7c88b32392",
+//   "drk": 1,
+//   "top": 1,
+//   "bot": 1,
+//   "hs": []
+//   }
+//   ],
+//   "tooltips": {
+//   "loading": "正在加载...",
+//   "previous": "上一个图像",
+//   "next": "下一个图像",
+//   "walle": "此图片不能下载用作壁纸。",
+//   "walls": "下载今日美图。仅限用作桌面壁纸。"
+//   }
+//   }
--- src/newtab/App/App.vue
+++ src/newtab/App/App.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="main_app" :style="{ background: `url(${bgLink})` }">
-    <div class="head-bar"></div>
-    <div class="mid-search"></div>
+    <div class="head-bar">
+      <div class="time-part">
+        {{ nowTime }}
+      </div>
+      <div class="weather-part">
+        {{ localWeather }}
+      </div>
+    </div>
+    <div class="mid-search">
+      <input class="input-s" type="text" v-model="inputData">
+    </div>
     <div class="bot-links"></div>
   </div>
 </template>
@@ -15,15 +24,39 @@ export default {
   data() {
     return {
       bgLink: "",
+      date: new Date(),
+      updateTimer: null,
+      localWeather: "晴天",
+      inputData:"plz input"
     };
   },
-  mounted() {
-    var chrome = window.chrome;
-    var bg = chrome.extension.getBackgroundPage();
-    this.bgLink = bg._getBackgroundImageFromBing();
+  created() {
+    this._initTimer();
+    this._initBackground();
   },
-  computed: {},
-  methods: {},
+  computed: {
+    nowTime() {
+      let date = this.date;
+      return `${date.getHours()}:${date.getMinutes()}`;
+    },
+  },
+  methods: {
+    _initTimer() {
+      this.date = new Date();
+      clearInterval(this.updateTimer);
+      this.updateTimer = setInterval(() => {
+        this.date = new Date();
+      }, 1000);
+    },
+    _initBackground() {
+      var bg = window.chrome.extension.getBackgroundPage();
+      console.log("bglink " + this.bgLink);
+      bg._getBackgroundImageFromBing().then((res) => {
+        this.bgLink = res;
+      });
+      console.log("bglink " + this.bgLink);
+    },
+  },
 };
 </script>
 
@@ -34,6 +67,48 @@ body {
 .main_app {
   width: 100%;
   height: 100vh;
-  background: 100% 100% no-repeat;
+  background-repeat: no-repeat;
+  background-position: 0% 0%;
+  display: flex;
+  justify-content: center;
+  position: relative;
+}
+/* .main_app:after {
+  content: "";
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  background: inherit;
+  filter: blur(1px);
+  z-index: 2;
+} */
+.head-bar {
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.time-part {
+  color: #fff;
+  padding: 50px 10px 0 50px;
+  font-size: 40px;
+  z-index: 10;
+}
+.weather-part {
+  font-size: 40px;
+  padding: 50px 10px 0 10px;
+  color: #fff;
+  z-index: 10;
+}
+.mid-search{
+  padding-top: 300px;
+  z-index: 10;
+}
+.input-s{
+  padding: 10px 25px;
 }
 </style>
